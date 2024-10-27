d5cdef3b740c808295d2010bb4c379bc2c08af90
arkstudiodevelopers <nicksonogm@gmail.com>
2024-10-27 19:27:40 +0300

    more sql practice

--- frontend/src/components/Mysql.sql
+++ frontend/src/components/Mysql.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS "My_Test_DB";
+DROP DATABASE IF EXISTS `My_Test_DB`;
 
-CREATE DATABASE "My_Test_DB";
+CREATE DATABASE `My_Test_DB`;
 use My_Test_DB;
 
 CREATE TABLE employee_demographics (
@@ -22,17 +22,17 @@ CREATE TABLE employee_slary(
 
 INSERT INTO employee_demographics (employee_id, first_name, last_name, age, gender, birth_date)
 VALUES
-(1,'Leslie', 'Knope', 44, 'Female','1979-09-25'),
-(3,'Tom', 'Haverford', 36, 'Male', '1987-03-04'),
-(4, 'April', 'Ludgate', 29, 'Female', '1994-03-27'),
-(5, 'Jerry', 'Gergich', 61, 'Male', '1962-08-28'),
-(6, 'Donna', 'Meagle', 46, 'Female', '1977-07-30'),
-(7, 'Ann', 'Perkins', 35, 'Female', '1988-12-01'),
-(8, 'Chris', 'Traeger', 43, 'Male', '1980-11-11'),
-(9, 'Ben', 'Wyatt', 38, 'Male', '1985-07-26'),
-(10, 'Andy', 'Dwyer', 34, 'Male', '1989-03-25'),
-(11, 'Mark', 'Brendanawicz', 40, 'Male', '1983-06-14'),
-(12, 'Craig', 'Middlebrooks', 37, 'Male', '1986-07-27');
+(1,'Leslie', 'Knope', 44, '1979-09-25'),
+(3,'Tom', 'Haverford', 36, '1987-03-04'),
+(4, 'April', 'Ludgate', 29,'1994-03-27'),
+(5, 'Jerry', 'Gergich', 61,'1962-08-28'),
+(6, 'Donna', 'Meagle', 46, '1977-07-30'),
+(7, 'Ann', 'Perkins', 35, '1988-12-01'),
+(8, 'Chris', 'Traeger', 43, '1980-11-11'),
+(9, 'Ben', 'Wyatt', 38, '1985-07-26'),
+(10, 'Andy', 'Dwyer', 34,'1989-03-25'),
+(11, 'Mark', 'Brendanawicz', 40, '1983-06-14'),
+(12, 'Craig', 'Middlebrooks', 37, '1986-07-27');
 
 INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
 VALUES
@@ -63,3 +63,36 @@ VALUES
 ('Healthcare'),
 ('Library'),
 ('Finance');
+
+
+
+#this is a comment
+
+select * from my_test_db.employee_demographics where age <30;
+
+select * from my_test_db.employee_demographics where age <70 && first_name = 'Tom';
+
+ select first_name, last_name, age from my_test_db.employee_demographics where first_name LIKE 'd%';
+
+
+DROP DATABASE IF EXISTS `Trial_DB`;
+CREATE DATABASE `Trial_DB`;
+
+USE `Trial_DB`;
+
+CREATE TABLE user_data (
+user_ID INT auto_increment primary KEY,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+email varchar(100)
+);
+
+INSERT INTO user_data ( first_name, last_name, email) values
+('Nick', 'ojiem', 'nixon@gmail.com'),
+('Grace', 'Emmy', 'grace@gmail.com');
+
+describe user_data;
+
+select * FROM trial_db.user_data;
+
+
